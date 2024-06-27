08f019499fb796be8377156ba41c4bd5afdaa801
pharos-lab <1i6h7h0u53.lighthouse@gmail.com>
2024-06-27 10:00:08 +0200

    mastermind: add color and undo color visible on board

--- src/pages/Mastermind.vue
+++ src/pages/Mastermind.vue
@@ -2,10 +2,10 @@
     <main class="flex items-center flex-col">
         <div class="mastermind-grid bg-slate-200 divide-slate-300 divide-y">
             <div class="row flex divide-x divide-slate-300" v-for="n in 10">
-                <div class="combination flex gap-4 p-2">
+                <div class="combination flex gap-4 p-2" ref="combinations">
                     <div class="size-7 rounded-full border bg-white" v-for="n in 4"></div>
                 </div>
-                <div class="result flex flex-wrap justify-center w-14 p-2">
+                <div class="result flex flex-wrap justify-center w-14 p-2" ref="results">
                     <div class="size-4 rounded-full border bg-white" v-for="n in 4"></div>
                 </div>
             </div>
@@ -22,6 +22,7 @@
         <div class="flex gap-4 justify-center mt-4">
             <button class="px-3 py-2 rounded bg-slate-100" @click="undo">undo</button>
             <button class="px-3 py-2 rounded bg-slate-100">go</button>
+            <button  class="px-3 py-2 rounded bg-slate-100" @click="test">test</button>
         </div>
     </main>
 </template>
@@ -30,16 +31,40 @@
     import { ref } from 'vue'
 
     const combination = ref([])
+    const combinations = ref()
+    const results = ref()
+    const currentLine = ref(9)
+    const lineIndex = ref(0)
+    const colorMap = {
+        'red': 'bg-red-500',
+        'orange': 'bg-orange-500',
+        'green': 'bg-green-500',
+        'blue': 'bg-blue-500',
+        'yellow': 'bg-yellow-500',
+        'purple': 'bg-purple-500'
+    }
+    const lastColor = ref('')
 
     const add = (color) => {
-        if (combination.value.length < 4) {
+        if (lineIndex.value < 4) {
+            lastColor.value = color
             combination.value.push(color)
+            combinations.value[currentLine.value].children[lineIndex.value].classList.add(colorMap[color])
+            lineIndex.value++
         }
     }
 
     const undo = () => {
         if (combination.value.length >= 1) {
+            combinations.value[currentLine.value].children[lineIndex.value - 1].classList.remove(colorMap[lastColor.value])
             combination.value.pop()
+            lineIndex.value--
+            lastColor.value = combination.value.at(-1)
+
         }
+    }
+
+    const test = () => {
+        console.log(lineIndex.value)
     }
 </script>
