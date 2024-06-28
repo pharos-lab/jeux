6907b896697c817411c3462db277a09fb903f1b9
pharos-lab <1i6h7h0u53.lighthouse@gmail.com>
2024-06-28 10:08:04 +0200

    mastermind: refactor to use one combination list and one result list

--- src/pages/Mastermind.vue
+++ src/pages/Mastermind.vue
@@ -1,12 +1,16 @@
 <template>
     <main class="flex items-center flex-col">
+        {{ combinations }}
+        {{ results }}
         <div class="mastermind-grid bg-slate-300 divide-slate-400 divide-y">
-            <div class="row flex divide-x divide-slate-300" v-for="n in 10">
+            <div class="row flex divide-x divide-slate-300" v-for="line in 10">
                 <div class="combination flex gap-4 p-2" ref="combinationsGrid">
-                    <div class="size-7 rounded-full border " v-for="n in 4"></div>
+                    <div class="size-7 rounded-full border " v-for="combinationCase in 4"
+                        :class="colorMap[combinations[line -1][combinationCase - 1]]"></div>
                 </div>
                 <div class="result flex flex-wrap justify-center w-14 p-2" ref="resultsGrid">
-                    <div class="size-4 rounded-full border" v-for="n in 4"></div>
+                    <div class="size-4 rounded-full border" v-for="resultCase in 4"
+                        :class="colorMap[results[line -1 ][resultCase - 1]]"></div>
                 </div>
             </div>
         </div>
@@ -21,8 +25,8 @@
         </div>
         <div class="flex gap-4 justify-center mt-4">
             <button class="px-3 py-2 rounded bg-slate-100" @click="undo">undo</button>
-            <button class="px-3 py-2 rounded bg-slate-100"@click="submitCombination">go</button>
-            <button  class="px-3 py-2 rounded bg-slate-100" @click="test">test</button>
+            <button class="px-3 py-2 rounded bg-slate-100" @click="submitCombination">go</button>
+            <button class="px-3 py-2 rounded bg-slate-100" @click="test">test</button>
         </div>
         <div class="self-stretch p-4">
             <pre>combination: {{ combination }}
@@ -33,100 +37,86 @@
 </template>
 
 <script setup>
-    import { ref } from 'vue'
+import { ref } from 'vue'
 
-    const combination = ref([])
-    const combinationToFind = ['red', 'blue', 'green', 'red']
-    const combinations = ref([])
-    const combinationsGrid = ref()
-    const results = ref([])
-    const resultsGrid = ref()
-    const currentResult = ref({
-        'good': 0,
-        'bad': 0
+const combination = ref([])
+const combinationToFind = ['red', 'blue', 'green', 'red']
+const combinations = ref([...Array(10)].map(line => Array(4).fill('')))
+const combinationsGrid = ref()
+const results = ref([...Array(10)].map(line => Array(4).fill('')))
+const resultsGrid = ref()
+const currentResult = ref([])
+const currentLine = ref(9)
+const combinationIndex = ref(0)
+const resultIndex = ref(0)
+const colorMap = {
+    'red': 'bg-red-500',
+    'orange': 'bg-orange-500',
+    'green': 'bg-green-500',
+    'blue': 'bg-blue-500',
+    'yellow': 'bg-yellow-500',
+    'purple': 'bg-purple-500',
+    'good': 'bg-white',
+    'bad': 'bg-black',
+    '': ''
+}
+const lastColor = ref('')
+
+const add = (color) => {
+    if (combinationIndex.value < 4 && currentLine.value >= 0) {
+        lastColor.value = color
+        combinations.value[currentLine.value][combinationIndex.value] = color
+        combinationIndex.value++
+    }
+}
+
+const undo = () => {
+    if (combinationIndex.value >= 1) {
+        combinationIndex.value--
+        combinations.value[currentLine.value][combinationIndex.value] = ''
+        lastColor.value = combination.value.at(-1)
+    }
+}
+
+const submitCombination = () => {
+    if (combinationIndex.value == 4) {
+        const userCombination = [...combinations.value[currentLine.value]]
+        const cpuCombination = [...combinationToFind]
+        goodPlacePawn(userCombination, cpuCombination)
+        wrongPlacePawn(userCombination, cpuCombination)
+        combinationIndex.value = 0
+        resultIndex.value = 0
+        currentLine.value--
+    }
+}
+
+const goodPlacePawn = (userCombination, cpuCombination) => {
+    userCombination.forEach((element, index) => {
+        if (element == cpuCombination[index]) {
+            userCombination[index] = 'a'
+            cpuCombination[index] = 'b'
+            results.value[currentLine.value][resultIndex.value] = 'good'
+            resultIndex.value++
+        }
     })
-    const currentLine = ref(9)
-    const lineIndex = ref(0)
-    const colorMap = {
-        'red': 'bg-red-500',
-        'orange': 'bg-orange-500',
-        'green': 'bg-green-500',
-        'blue': 'bg-blue-500',
-        'yellow': 'bg-yellow-500',
-        'purple': 'bg-purple-500'
-    }
-    const lastColor = ref('')
+}
 
-    const add = (color) => {
-        if (lineIndex.value < 4) {
-            lastColor.value = color
-            combination.value.push(color)
-            combinationsGrid.value[currentLine.value].children[lineIndex.value].classList.add(colorMap[color])
-            lineIndex.value++
+const wrongPlacePawn = (userCombination, cpuCombination) => {
+    userCombination.forEach((element, index) => {
+        if (cpuCombination.includes(element)) {
+            userCombination[index] = 'x'
+            cpuCombination[cpuCombination.indexOf(element)] = 'y'
+            results.value[currentLine.value][resultIndex.value] = 'bad'
+            resultIndex.value++
         }
-    }
+    })
+}
 
-    const undo = () => {
-        if (lineIndex.value >= 1) {
-            combinationsGrid.value[currentLine.value].children[lineIndex.value - 1].classList.remove(colorMap[lastColor.value])
-            combination.value.pop()
-            lineIndex.value--
-            lastColor.value = combination.value.at(-1)
-
-        }
-    }
-
-    const submitCombination = () => {
-        if (lineIndex.value == 4) {
-            const userCombination = [...combination.value]
-            const cpuCombination = [...combinationToFind]
-            goodPlacePawn(userCombination, cpuCombination)
-            wrongPlacePawn(userCombination, cpuCombination)
-            drawResult()
-            lineIndex.value = 0
-            currentLine.value--
-            currentResult.value = {
-            'good': 0,
-            'bad': 0
-            }
-            combination.value = []
-        }
-    }
-
-    const goodPlacePawn = (userCombination, cpuCombination) => {
-        userCombination.forEach((element, index) => {
-            if (element == cpuCombination[index]) {
-                userCombination[index] = 'a'
-                cpuCombination[index] = 'b'
-                currentResult.value['good']++
-            }
-        })
-    }
-
-    const wrongPlacePawn = (userCombination, cpuCombination) => {
-        userCombination.forEach((element, index) => {
-            if (cpuCombination.includes(element)) {
-                userCombination[index] = 'x'
-                cpuCombination[cpuCombination.indexOf(element)] = 'y'
-                currentResult.value['bad']++
-            }
-        })
-    }
-
-    const drawResult = () => {
-        for (let i = 0; i<currentResult.value['good']; i++) {
-            resultsGrid.value[currentLine.value].children[i].classList.add('bg-gray-200')
-        }
-        for (let i = 0; i<currentResult.value['bad']; i++) {
-            resultsGrid.value[currentLine.value].children[i + currentResult.value['good']].classList.add('bg-black')
-        }
-    }
-
-    const test = () => {
-        console.table(
-            'current line: ' + currentLine.value,
-            'current Index: '+ lineIndex.value,
-            'currrent result: ' + currentResult.value
-        )
-    }
+const test = () => {
+    console.table(
+        'current line: ' + currentLine.value,
+        'current Index: ' + combinationIndex.value,
+        'currrent result: ' + currentResult.value
+    )
+}
 </script>
