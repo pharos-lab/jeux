effc6353b447dcb42d569c02413b349be0e0fadc
pharos-lab <1i6h7h0u53.lighthouse@gmail.com>
2024-07-01 08:09:08 +0200

    add cpu mode to play against cpu

--- src/pages/Mastermind.vue
+++ src/pages/Mastermind.vue
@@ -1,21 +1,21 @@
 <template>
     <main class="flex items-center flex-col">
-        {{ combinations }}
-        {{ results }}
+        <button @click="mode = 'cpu'" v-show="mode == 'user'">change to cpu</button>
+        <button @click="mode = 'user'" v-show="mode == 'cpu'">change to user</button>
         <div class="mastermind-grid bg-slate-300 divide-slate-400 divide-y">
             <div class="row flex divide-x divide-slate-300" v-for="line in 10">
                 <div class="combination flex gap-4 p-2" ref="combinationsGrid">
                     <div class="size-7 rounded-full border " v-for="combinationCase in 4"
-                        :class="colorMap[combinations[line -1][combinationCase - 1]]"></div>
+                        :class="colorMap[combinations[line - 1][combinationCase - 1]]"></div>
                 </div>
                 <div class="result flex flex-wrap justify-center w-14 p-2" ref="resultsGrid">
                     <div class="size-4 rounded-full border" v-for="resultCase in 4"
-                        :class="colorMap[results[line -1 ][resultCase - 1]]"></div>
+                        :class="colorMap[results[line - 1][resultCase - 1]]"></div>
                 </div>
             </div>
         </div>
 
-        <div class="mastermind-choice flex gap-4 mt-8">
+        <div class="mastermind-choice flex gap-4 mt-8" v-show="mode == 'user'">
             <button @click="add('red')" class="bg-red-500 rounded size-7"></button>
             <button @click="add('orange')" class="bg-orange-500 rounded size-7"></button>
             <button @click="add('green')" class="bg-green-500 rounded size-7"></button>
@@ -23,15 +23,28 @@
             <button @click="add('yellow')" class="bg-yellow-500 rounded size-7"></button>
             <button @click="add('purple')" class="bg-purple-500 rounded size-7"></button>
         </div>
-        <div class="flex gap-4 justify-center mt-4">
+
+        <div class="mastermind-choice flex gap-4 mt-8" v-show="mode == 'cpu'">
+            <button @click="add('red')" class="bg-slate-200 rounded size-7"></button>
+            <button @click="add('orange')" class="bg-black rounded size-7"></button>
+        </div>
+
+
+
+        <div class="flex gap-4 justify-center mt-4" v-show="mode == 'user'">
             <button class="px-3 py-2 rounded bg-slate-100" @click="undo">undo</button>
-            <button class="px-3 py-2 rounded bg-slate-100" @click="submitCombination">go</button>
-            <button class="px-3 py-2 rounded bg-slate-100" @click="test">test</button>
+            <button class="px-3 py-2 rounded bg-slate-100" @click="submitCombination" >go</button>
+        </div>
+
+        <div class="flex gap-4 justify-center mt-4" v-show="mode == 'cpu'">
+            <button class="px-3 py-2 rounded bg-slate-100" @click="undoResult">undo</button>
+            <button class="px-3 py-2 rounded bg-slate-100" @click="submitResult" >go result</button>
         </div>
         <div class="self-stretch p-4">
-            <pre>combination: {{ combination }}
-            </pre>
+
             <pre>to find: {{ combinationToFind }}</pre>
+            {{ combinations }}
+        {{ results }}
         </div>
     </main>
 </template>
@@ -39,6 +52,7 @@
 <script setup>
 import { ref } from 'vue'
 
+const mode = ref('user')
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
 const combinationToFind = [...Array(4)].map(color => colors[Math.floor(Math.random() * 6)])
 const combinations = ref([...Array(10)].map(line => Array(4).fill('')))
